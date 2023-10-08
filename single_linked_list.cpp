8421db136aba9645ea24b40cfdb7e8e1705ccdc7
sangeetha <jadamalsangeetha@gmail.com>
2023-10-08 23:29:13 +0530

    single-lisked-list

---
+++ single_linked_list.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+void cerate();
+void display();
+struct link
+{
+    int data;
+    link *next;
+};
+struct link *start = NULL;
+struct link *node;
+struct link *current;
+void create()
+{
+    int n;
+    cout<<"Enter how many nodes you want ot craete : ";
+    cin>>n;
+    cout<<"Enter all the elements : ";
+    for(int i=0;i<n;i++)
+    {
+        node=(struct link *)malloc(sizeof(struct link));
+        cin>>node->data;
+        node->next=NULL;
+        if(start==NULL)
+        {
+            start = node;
+            current = node;
+        }
+        else{
+            current->next = node;
+            current = current->next;
+        }
+    }
+}
+void display()
+{
+    struct link *ptr=start;
+    while(ptr!=NULL)
+    {
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+}
+int main()
+{
+    create();
+    display();
+    return 0;
+}
